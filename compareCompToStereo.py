715e46a5e3f9ea4a529f40bf3be85ca924cb9d1e
David Dulac <dpdulac@hotmail.com>
2018-09-19 18:49:11 +0200

    compare test

--- compareCompToStereo.py
+++ compareCompToStereo.py
@@ -10,13 +10,15 @@
 
 """
 """
-    Compare the technical aprouved compo_comp shot to the technical aprouved compo_stereo shot (only left eye)
+    comand line: compareCompToStereo 10-20 30-40 ..... option -rv to display in rv
+    Compare the technical approved compo_comp shot to the technical approved compo_stereo shot (only left eye)
+    if error a difference image will be calculated and put in the /tmp/sequence_shot directory
     
 """
 
 
 import OpenImageIO as oiio
-import os, pprint, errno, argparse, sys
+import os, pprint, errno, argparse, glob
 from sgtkLib import tkutil, tkm
 
 _USER_ = os.environ['USER']
@@ -26,7 +28,7 @@ _FONT_ = 'LiberationSans-Italic'
 tk, sgw, project = tkutil.getTk(fast=True, scriptName=_USER_)
 sg = sgw._sg
 
-def findShots( seq=40, shot = 135):
+def findShots( seq=40, shot = 135,status ='chk'):
     # format the input
     seqShot = 's'+str(seq).zfill(4)+'_p'+str(shot).zfill(4)
 
@@ -36,7 +38,7 @@ def findShots( seq=40, shot = 135):
         ['entity', 'type_is', 'Shot'],
         ['entity.Shot.code', 'is', seqShot],
         ['sg_task', 'name_is', 'compo_comp'],
-        ['sg_status_list', 'is', 'cmpt'],
+        ['sg_status_list', 'is', status],
     ]
 
     res = {}
@@ -55,7 +57,7 @@ def findShots( seq=40, shot = 135):
     try:
         res['framePathCompoComp']
     except:
-        print("\x1b[0;31;40m"+'no shot for ' + seqShot + ' in compo_stereo marked as cmpt as been found'+"\x1b[0m")
+        print("\x1b[0;31;40m"+'no shot for ' + seqShot + ' in compo_stereo marked '+status+' has been found'+"\x1b[0m")
         exit(0)
 
     # filter for compo_stereo
@@ -90,6 +92,11 @@ def compareStereo(res={},rv=False):
     cutOut = res['cutOut']
 
     path = _OUTPATH_+'/'+res['name']+'/'
+    # delete the old files in the directory
+    if os.path.isdir(path):
+        files = glob.glob(path+'*')
+        for f in files:
+            os.remove(f)
     diff = False
     matchStatus = '\x1b[0;32;40m match\x1b[0m'
     wrongStatus = "\x1b[0;31;40m don't match\x1b[0m"
@@ -122,15 +129,16 @@ def compareStereo(res={},rv=False):
         print path
         os.system('rv '+ path )
 
-def doCompare(imgA='',imgB='',path = ''):
+def doCompare(imgA='',imgB='',path = '',errorTol=0.01, warnTol = 1.0):
 
     comp = oiio.CompareResults()
 
-    oiio.ImageBufAlgo.compare(oiio.ImageBuf(imgA), oiio.ImageBuf(imgB), 1/100.0, 1.0, comp)
+    oiio.ImageBufAlgo.compare(oiio.ImageBuf(imgA), oiio.ImageBuf(imgB), errorTol, warnTol, comp)
 
     if comp.nwarn == 0 and comp.nfail == 0:
         return True
     else:
+        # if path doesn't exist create it
         if not os.path.isdir(path):
             os.makedirs(path)
         pathFrame = path+ imgA[imgA.rfind('/')+1:imgA.rfind('.exr')]+'.jpg'
@@ -145,34 +153,40 @@ def get_args():
     #Assign description to the help doc
     parser = argparse.ArgumentParser(description = "compare the compo_comp left images to compo_stereo left images for the shot")
     #shot argument
-    parser.add_argument('sequences', type=str,nargs='*', help='seq number follow by shot number can have multiple seq. Sequence and shot need to be separated by a - (i.e: 40-130 10-10 180-10)')
-    parser.add_argument('--rv', '-rv', action='store_true', help=' do not display the resulting image(s) in rv')
+    parser.add_argument('sequences', type=str,nargs='*', help="seq number follow by shot number (can have multiple seq). Sequence and shot need to be separated by a '-' (i.e: compareCompToStereo 40-130 10-10 180-10)")
+    parser.add_argument('--rv', '-rv', action='store_true', help=' display the resulting image(s) in rv')
+    parser.add_argument('--cmpt', '-cmpt', action='store_true', help=' use the complete version of stereo left')
     args = parser.parse_args()
     seqShotNumber = args.sequences
     rv = args.rv
-    if len(seqShotNumber) > 1:
-        rv = False
 
     seqShotToDo = []
     for seqShots in seqShotNumber:
-        if seqShots.find('-') < 0:
+        if seqShots.find('-') < 0 or seqShots.find('-') > len(seqShots)-1:
             print "\x1b[0;31;40m usage: sequence number follow by a '-' then shot number (i.e: 430-10)"
             print seqShots+ ' will not be include in the list\x1b[0m'
         else:
             seqShotToDo.append(seqShots)
-    try:
-        len(seqShotToDo) > 0
-    except:
 
-        print 'seq number follow by shot number can have multiple seq. Sequence and shot need to be separated by a - (i.e: 40-130 10-10 180-10)'
+    if len(seqShotToDo) < 1:
+        print "\x1b[0;31;40m\nlist is empty!!!, usage:\nseq number follow by shot number (can have multiple seq). Sequence and shot need to be separated by a '-' (i.e: compareCompToStereo 40-130 10-10 180-10)\x1b[0m"
         exit(0)
 
-    return seqShotToDo,rv
+    if len(seqShotToDo) > 1:
+        rv = False
+
+    status = ''
+    if args.cmpt:
+        status = 'cmpt'
+    else:
+        status = 'chk'
+
+    return seqShotToDo,rv,status
 
 def main():
-    seqShot , rv= get_args()
+    seqShot , rv, status= get_args()
     for shot in seqShot:
-        res = findShots(shot.split('-')[0], shot.split('-')[1])
+        res = findShots(shot.split('-')[0], shot.split('-')[1],status)
         #pprint.pprint(res)
         compareStereo(res,rv)
 
