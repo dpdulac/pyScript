e2c3a207b81f95be493c77166794e02198e96e8e
David Dulac <dpdulac@hotmail.com>
2020-12-23 09:53:56 +0100

    test

--- sgTest.py
+++ sgTest.py
@@ -26,19 +26,28 @@ def findAllSequence(all = False):
         ['project', 'is', {'type':'Project', 'id':project.id}],
     ]
     seqShots =[]
+    desc = []
+    res = {}
     #get all the name of the shots and masters from the sequence
     for v in sg.find('Sequence',filters,['entity','code','description']):
         seq = v['code']
+        des = v['description']
         if all:
             if not seq in seqShots:
-                seqShots.append(seq)
+                res[seq] = des
         else:
             if int(seq[seq.find('s')+1:]) <9000:
                 if not seq in seqShots:
-                    seqShots.append(seq)
+                    res[seq] = des
             else:
                 print  seq + ' will not be included'
-    seqShots=sorted(seqShots)
-    return seqShots
+    #seqShots=sorted(seqShots)
+    # res = {}
+    # for i in range(len(seqShots)):
+    #     res[seqShots[i]] = desc[i]
+    for item in sorted(res.keys()):
+        print item,res[item]
+
+    return res
 
 print findAllSequence()
